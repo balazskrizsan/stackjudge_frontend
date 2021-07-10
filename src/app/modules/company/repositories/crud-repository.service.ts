181125c471554ce2f929790d68391d2e045fa760
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-07-10 02:31:21 +0200

    crud-repository.service remove

--- src/app/modules/company/repositories/crud-repository.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from '@angular/core';
-import {AbstractApiRepository} from '../../../repositories/abstract-api-repository';
-import {HttpService} from '../../../services/http-service';
-
-export abstract class CrudRepository extends AbstractApiRepository {
-  protected constructor(httpService: HttpService) {
-    super(httpService);
-  }
-}
