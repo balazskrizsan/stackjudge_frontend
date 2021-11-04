b4c3777858bef38c8b56de55f0b57088e7b927ee
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-11-04 15:34:07 +0100

    environment.local-vagrant env added

---
+++ src/environments/environment.local-vagrant.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  backend: {
+    api: {
+      host: 'https://192.168.33.10:81'
+    },
+    account: {
+      fbLoginAndRegistrationUrl: 'https://192.168.33.10:81/account/facebook/registration-and-login'
+    }
+  },
+  cdn: {
+    host: 'https://stackjudge-cdn-dev-eu-central-1.s3.eu-central-1.amazonaws.com/'
+  }
+};
